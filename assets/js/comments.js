c84e4a304993c5937230ec18b10fa14a42f586c4
wangyunzi <xueq695@gmail.com>
2022-12-19 17:35:31 +0800

    🌈2022-12-19 17:35:30

--- assets/js/comments.js
+++ assets/js/comments.js
@@ -1,16 +1,18 @@
-window.addEventListener("DOMContentLoaded", event => {
-    {{ if .Site.Params.autoLoadComments }}
-        if (typeof loadComments === 'function') {
-            loadComments()
-        }
-    {{ else }}
-        const commentsToggle = document.getElementById('load-comments');
+<script>
+  var disqusButton = document.getElementById('disqus-button');
+  var gitalkButton = document.getElementById('gitalk-button');
 
-        if (commentsToggle !== null) {
-            commentsToggle.addEventListener('click', function () {
-                loadComments();
-                this.style = "display: none";
-            });
-        }
-    {{  end }}
-}, {once: true});
+  disqusButton.addEventListener('click', function() {
+    disqusButton.classList.add('active');
+    gitalkButton.classList.remove('active');
+    document.getElementById('disqus_thread').style.display = 'block';
+    document.getElementById('gitalk-container').style.display = 'none';
+  });
+
+  gitalkButton.addEventListener('click', function() {
+    gitalkButton.classList.add('active');
+    disqusButton.classList.remove('active');
+    document.getElementById('gitalk-container').style.display = 'block';
+    document.getElementById('disqus_thread').style.display = 'none';
+  });
+</script>
